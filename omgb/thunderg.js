32e211786ec66bd80f1f81ca1b84cbb2b80714e3
Giorgio Andronico <techdevelop.x@gmail.com>
2012-01-22 13:20:11 +0100

    Add support for my nightlies

---
+++ omgb/thunderg.js
@@ -0,0 +1,12 @@
+[
+{
+"device"="thunderg",
+"version"="1.2.3",
+"date"="01-01",
+"name"="OMGB_thunderg-1.2.3.zip",
+"url"="http://r2doesinc.bitsurge.net/omgb/heroc/OMGB_heroc-1.2.2.zip",
+"installable"="false",
+"description"="First release from Bytecode for LG-P500."
+},
+ 
+]
